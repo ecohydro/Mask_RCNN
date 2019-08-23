5d2b1b8bde48c77bd94bbf40e75828f151cd39d4
Ryan Avery <ravery@ucsb.edu>
2019-08-22 18:20:52 -0700

    factored out and edited funcs that depended on single state to use dask delayed, runs

--- cropmask/preprocess.py
+++ cropmask/preprocess.py
@@ -19,8 +19,49 @@ from cropmask.misc import parse_yaml, make_dirs
 from cropmask import sequential_grid, label_prep
 from cropmask import io_utils 
 import us
+import time
 
 random.seed(42)
+
+def setup_dirs(param_path):
+    """
+    This folder structure is used for each unique pre processing and modeling
+    workflow and is made unique by specifying a unique DATASET name
+    or ROOT path (if working on a different container.). 
+
+    ROOT should be the path to the azure container mounted with blobfuse, 
+    and should already exist. The RESULTS folder should be created in a folder named from param["results"], and this should also already exist.
+    """
+    
+    params = parse_yaml(param_path)
+    
+    # the folder structure for the unique run
+    ROOT = params['dirs']["root"]
+    TMP = params['dirs']["tmp"]
+    DATASET = os.path.join(ROOT, params['dirs']["dataset"])
+    SCENE = os.path.join(DATASET, params['dirs']["scene"])
+    TRAIN = os.path.join(DATASET, params['dirs']["train"])
+    TEST = os.path.join(DATASET, params['dirs']["test"])
+    GRIDDED_IMGS = os.path.join(TMP, params['dirs']["gridded_imgs"])
+    GRIDDED_LABELS = os.path.join(TMP, params['dirs']["gridded_labels"])
+    NEG_BUFFERED = os.path.join(DATASET, params['dirs']["neg_buffered_labels"])
+    RESULTS = os.path.join(ROOT, params['dirs']["results"], params['dirs']["dataset"])
+
+    directory_list = [
+            TMP,
+            DATASET,
+            SCENE,
+            TRAIN,
+            TEST,
+            GRIDDED_IMGS,
+            GRIDDED_LABELS,
+            NEG_BUFFERED,
+            RESULTS,
+        ]
+    if os.path.exists(os.path.join(ROOT, params['dirs']["results"])) == False:
+        os.mkdir(os.path.join(ROOT, params['dirs']["results"]))
+    make_dirs(directory_list)
+    return directory_list
 
 class PreprocessWorkflow():
     """
@@ -50,7 +91,8 @@ class PreprocessWorkflow():
         self.rasterized_label_path = ''
         self.band_list = [] # the band indices
         self.meta = {} # meta data for the scene
-        self.chip_ids = [] # list of chip ids of form [scene_id]_[random number]
+        self.chip_img_paths = [] # list of chip ids of form [scene_id]_[random number]
+        self.chip_label_paths = [] # list of chip labels of form [scene_id]_[random number]
         self.small_area_filter = params['label_vals']['small_area_filter']
         self.neg_buffer = params['label_vals']['neg_buffer']
         self.ag_class_int = params['label_vals']['ag_class_int'] # TO DO, not implemented but needs to be for multi class
@@ -83,32 +125,6 @@ class PreprocessWorkflow():
             if list(band.values())[0] == True:
                 self.band_list.append(str(i+1))
         return self.band_list
-
-    def setup_dirs(self):
-        """
-        This folder structure is used for each unique pre processing and modeling
-        workflow and is made unique by specifying a unique DATASET name
-        or ROOT path (if working on a different container.). 
-        
-        ROOT should be the path to the azure container mounted with blobfuse, 
-        and should already exist. The RESULTS folder should be created in a folder named from param["results"], and this should also already exist.
-        """
-
-        directory_list = [
-                self.TMP,
-                self.DATASET,
-                self.SCENE,
-                self.TRAIN,
-                self.TEST,
-                self.GRIDDED_IMGS,
-                self.GRIDDED_LABELS,
-                self.NEG_BUFFERED,
-                self.RESULTS,
-            ]
-        if os.path.exists(os.path.join(self.ROOT, self.params['dirs']["results"])) == False:
-            os.mkdir(os.path.join(self.ROOT, self.params['dirs']["results"]))
-        make_dirs(directory_list)
-        return directory_list
     
     def get_product_paths(self, band_list):
         # Load image
@@ -221,9 +237,9 @@ class PreprocessWorkflow():
         """
         nebraska_url = us.states.NE.shapefile_urls('state') #should be abstracted out for other regions to accept geojson
         gdf = io_utils.zipped_shp_url_to_gdf(nebraska_url)
-        chip_img_paths = sequential_grid.grid_images_rasterio_sequential(self.scene_path, self.GRIDDED_IMGS, gdf, output_name_template='tile_{}-{}.tif', grid_size=self.grid_size)
-        chip_label_paths = sequential_grid.grid_images_rasterio_sequential(self.rasterized_label_path, self.GRIDDED_LABELS, gdf, output_name_template='tile_{}-{}_label.tif', grid_size=self.grid_size)
-        return (chip_img_paths, chip_label_paths)      
+        self.chip_img_paths = sequential_grid.grid_images_rasterio_sequential(self.scene_path, self.GRIDDED_IMGS, gdf, output_name_template='tile_{}-{}.tif', grid_size=self.grid_size)
+        self.chip_label_paths = sequential_grid.grid_images_rasterio_sequential(self.rasterized_label_path, self.GRIDDED_LABELS, gdf, output_name_template='tile_{}-{}_label.tif', grid_size=self.grid_size)
+        return (self.chip_img_paths, self.chip_label_paths)      
                 
     def rm_mostly_empty(self, scene_path, label_path):
         """
@@ -245,12 +261,12 @@ class PreprocessWorkflow():
             os.remove(label_path)
             print("removed scene and label, {}% bad data".format(self.usable_threshold))
                 
-    def remove_from_gridded(self, chip_img_paths, chip_label_paths):
+    def remove_from_gridded(self):
                 
         def get_chip_id(string,ignore_string):
             return string.replace(ignore_string, '')
 
-        for img, label in zip(sorted(chip_img_paths, key=lambda x: get_chip_id(x,'.tif')), sorted(chip_label_paths, key=lambda x: get_chip_id(x,'_label.tif'))):
+        for img, label in zip(sorted(self.chip_img_paths, key=lambda x: get_chip_id(x,'.tif')), sorted(self.chip_label_paths, key=lambda x: get_chip_id(x,'_label.tif'))):
             self.rm_mostly_empty(img,label)
                 
     def move_chips_to_folder(self):
@@ -259,8 +275,8 @@ class PreprocessWorkflow():
         
         """
 
-        for chip_id in os.listdir(self.GRIDDED_IMGS):
-            chip_id = os.path.splitext(chip_id)[0]
+        for old_chip_path in self.chip_img_paths:
+            chip_id = os.path.basename(os.path.splitext(old_chip_path)[0])
             chip_folder_path = os.path.join(self.TRAIN, chip_id)
             if os.path.exists(chip_folder_path) == False:
                 os.mkdir(chip_folder_path)
@@ -270,7 +286,6 @@ class PreprocessWorkflow():
             mask_path = os.path.join(chip_folder_path, "mask")
             os.mkdir(new_chip_path)
             os.mkdir(mask_path)
-            old_chip_path = os.path.join(self.GRIDDED_IMGS, chip_id+'.tif')
             shutil.copyfile(old_chip_path, os.path.join(new_chip_path, chip_id + ".tif")) # moves the chips   
             os.remove(old_chip_path)
             
@@ -280,13 +295,14 @@ class PreprocessWorkflow():
         in each folder ID in train folder. If an image has no instances,
         saves it with a empty mask.
         """
-        for chip_id in os.listdir(self.TRAIN):
-            chip_label_path = os.path.join(self.GRIDDED_LABELS, chip_id+"_label.tif")
-            arr = skio.imread(chip_label_path)
+        for old_label_path in self.chip_label_paths:
+            
+            arr = skio.imread(old_label_path)
 
             blob_labels = label_prep.connected_components(arr)
             # for imgs with no instances, create empty mask
             if len(np.unique(blob_labels)) == 1:
+                chip_id = os.path.basename(os.path.splitext(old_label_path)[0])
                 mask_folder = os.path.join(self.TRAIN, chip_id, "mask")
                 skio.imsave(os.path.join(mask_folder, chip_id + ".tif"), blob_labels)
             else:
@@ -297,7 +313,7 @@ class PreprocessWorkflow():
                 mask_path = os.path.join(self.TRAIN, chip_id, "mask")
                 label_path = os.path.join(mask_path, label_name)
                 skio.imsave(label_path, label_arrs)
-            
+                
     def train_test_split(self):
         """Takes a sample of folder ids and copies them to a test directory
         from a directory with all folder ids. Each sample folder contains an 
@@ -316,37 +332,18 @@ class PreprocessWorkflow():
         train_df.to_csv(os.path.join(self.RESULTS, "train_ids.csv")) # used for reading in trainging and testing in the modeling step
         test_df.to_csv(os.path.join(self.RESULTS, "test_ids.csv"))
 
-    def get_arr_channel_mean(self, channel):
-        """
-        Calculate the mean of a given channel across all training samples.
-        """
-
-        means = []
-        train_list = next(os.walk(self.TRAIN))[1]
-        for i, fid in enumerate(train_list):
-            im_folder = os.path.join(self.TRAIN, fid, "image")
-            im_path = os.path.join(im_folder, os.listdir(im_folder)[0])
-            arr = skio.imread(im_path)
-            arr = arr.astype(np.float32, copy=False)
-            # added because no data values different for wv2 and landsat, need to exclude from mean
-            nodata_value = 0 # best to do no data masking up front and set bad qa bands to 0 rather than assuming 0 is no data. This is assumed from looking at no data values at corners being equal to 0
-            arr[arr == nodata_value] = np.nan
-            means.append(np.nanmean(arr[:, :, channel]))
-        return np.mean(means)
 
     def run_single_scene(self):
 
         start = time.time()
-
-        self.setup_dirs()
     
         band_list = self.yaml_to_band_index()
         
         product_list = self.get_product_paths(band_list)
         
-        meta, bounds = wflow.load_meta_and_bounds(product_list)
+        meta, bounds = self.load_meta_and_bounds(product_list)
 
-        scene = wflow.load_single_scene(product_list)
+        scene = self.load_single_scene(product_list)
         
         self.stack_and_save_bands()
         
@@ -360,14 +357,27 @@ class PreprocessWorkflow():
         
         self.connected_components()
         
-        self.train_test_split()
-        
-        print("channel means, put these in model_configs.py subclass")
-        for i in band_list:
-            print("Band index {} mean for normalization: ".format(i), self.get_arr_channel_mean(int(i)-1))
-                
-        print("preprocessing complete, ready to run model.")
-        
+        # train test split is done outside of this function to accomodate multiple scenes
+            
         stop = time.time()
         
         print(stop-start, " seconds")
+    
+def get_arr_channel_mean(chip_folder, channel):
+    """
+    Calculate the mean of a given channel across all training samples.
+    """
+
+    means = []
+    train_list = next(os.walk(chip_folder))[1]
+    for i, fid in enumerate(train_list):
+        im_folder = os.path.join(chip_folder, fid, "image")
+        im_path = os.path.join(im_folder, os.listdir(im_folder)[0])
+        arr = skio.imread(im_path)
+        arr = arr.astype(np.float32, copy=False)
+        # added because no data values different for wv2 and landsat, need to exclude from mean
+        nodata_value = 0 # best to do no data masking up front and set bad qa bands to 0 rather than assuming 0 is no data. This is assumed from looking at no data values at corners being equal to 0
+        arr[arr == nodata_value] = np.nan
+        means.append(np.nanmean(arr[:, :, channel]))
+    return np.mean(means)
+        
