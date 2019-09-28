a351d3ac8bf4ccbe8740dfd325a00e949a936e64
Ryan Avery <ravery@ucsb.edu>
2019-09-27 20:13:49 -0700

    add funcs to turn detection masks to geojson and updated runserver to write to blob

--- app/cropmask_api/runserver.py
+++ app/cropmask_api/runserver.py
@@ -6,7 +6,7 @@ from ai4e_service import APIService
 from sas_blob import SasBlob
 from PIL import Image
 import keras_detector
-from io import BytesIO
+from io import BytesIO, StringIO
 from os import getenv
 import uuid
 import sys
@@ -35,6 +35,8 @@ def process_request_data(request):
     try:
         # Attempt to load the body
         return_values['image_bytes'] = BytesIO(request.data)
+        # ToDo, also load the filename into a string to name outputs
+        # Landsat images store important date metadata in the filename, not binary content
     except:
         log.log_error('Unable to load the request data')   # Log to Application Insights
     return return_values
@@ -57,36 +59,50 @@ def detect(*args, **kwargs):
     ai4e_service.api_task_manager.UpdateTaskStatus(taskId, 'running - generate_detections')
 
     try:
-        arr_for_detection, image_for_drawing = keras_detector.open_image(image_bytes)
+        arr_for_detection, image_for_drawing, meta = keras_detector.open_image(image_bytes)
         ai4e_service.api_task_manager.UpdateTaskStatus(taskId, 'running - image opened, generating detections')
         #TO DO visualize masks
         rois, clsses, scores, masks = keras_detector.generate_detections(arr_for_detection)
 
         ai4e_service.api_task_manager.UpdateTaskStatus(taskId, 'rendering boxes')
 
+        print('runserver.py: detect() finished, rendering and saving result image and geojson...')
         # image is modified in place
         # here confidence_threshold is hardcoded, but you can ask that as a input from the request
         keras_detector.render_bounding_boxes(
             rois, scores, clsses, image_for_drawing, confidence_threshold=0.5)
 
-        print('runserver.py: detect(), rendering and saving result image...')
         # save the PIL Image object to a ByteIO stream so that it can be written to blob storage
         output_img_stream = BytesIO()
         image_for_drawing.save(output_img_stream, format='jpeg')
         output_img_stream.seek(0)
+
+        # save the geojson object to a ByteIO stream so that it can be written to Blob storage
+        geodf_result = keras_detector.mask_to_poly_gdf(masks, meta, do_transform=True)
+        output_geojson_stream = StringIO()
+        geodf_result.to_file(output_geojson_stream, driver="GeoJSON")
+        output_geojson_stream.seek(0)
 
         sas_blob_helper = SasBlob()
         # Create a unique name for a blob container
         container_name = str(uuid.uuid4()).replace('-','')
 
         # Create a writable sas container and return its url
-        sas_url = sas_blob_helper.create_writable_container_sas(
+        sas_url_img = sas_blob_helper.create_writable_container_sas(
+            getenv('STORAGE_ACCOUNT_NAME'), getenv('STORAGE_ACCOUNT_KEY'), container_name, blob_access_duration_hrs)
+
+        sas_url_geojson = sas_blob_helper.create_writable_container_sas(
             getenv('STORAGE_ACCOUNT_NAME'), getenv('STORAGE_ACCOUNT_KEY'), container_name, blob_access_duration_hrs)
 
         # Write the image to the blob
-        sas_blob_helper.write_blob(sas_url, 'task_{}_detect_output.jpg'.format(taskID), output_img_stream)
+        sas_blob_helper.write_blob(sas_url_img, 'task_{}_detect_output.jpg'.format(taskID), output_img_stream)
 
-        ai4e_service.api_task_manager.CompleteTask(taskId, 'completed - output written to: ' + sas_url)
+        ai4e_service.api_task_manager.CompleteTask(taskId, 'completed - img output written to: ' + sas_url_img)
+
+        # Write the geojson to the blob
+        sas_blob_helper.write_blob(sas_url_geojson, 'task_{}_detect_output.geojson'.format(taskID), output_geojson_stream)
+
+        ai4e_service.api_task_manager.CompleteTask(taskId, 'completed - geojson output written to: ' + sas_url_geojson)
         print('runserver.py: detect() finished.')
     except:
         log.log_exception(sys.exc_info()[0], taskId)
