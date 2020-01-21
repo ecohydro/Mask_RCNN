c66de61d3b198ce107afa5155b7711d55e92a033
Ryan Avery <ravery@ucsb.edu>
2020-01-21 15:23:28 -0800

    factored out coco convert steps from nb and reclassed detectron2 for validation metrics during training

--- cropmask/coco_convert.py
+++ cropmask/coco_convert.py
@@ -16,136 +16,41 @@ from sklearn.model_selection import train_test_split
 # funcs derived from https://github.com/waspinator/pycococreator/blob/d29534e36aad6c30d7e4dadd9f4f7b0e344a774c/pycococreatortools/pycococreatortools.py
 # and https://patrickwasp.com/create-your-own-coco-style-dataset/
 
-def create_coco_meta(set_id):
-    INFO = {
-        "description": "nebraska center pivots "+set_id,
-        "url": "https://github.com/waspinator/pycococreator",
+def create_coco_meta():
+    INFO_DICT = {
+        "description": "2005 nebraska center pivots derived from CALMIT dataset",
+        "url": "https://calmit.unl.edu/center-pivots-esri-shapefile-metadata-2005",
         "version": "0.1.0",
         "year": 2005,
         "contributor": "rbavery",
         "date_created": datetime.datetime.utcnow().isoformat(' ')
     }
 
-    LICENSES = [
+    LICENSE_DICT = [
         {
-            "id": 1,
             "name": "Attribution-NonCommercial-ShareAlike License",
             "url": "http://creativecommons.org/licenses/by-nc-sa/2.0/"
         }
     ]
 
-    CATEGORIES = [
+    PRESET_CATEGORIES = [
         {
             'id': 1,
-            'name': 'agriculture',
-            'supercategory': 'shape',
+            'name': 'center pivot',
+            'supercategory': 'agriculture',
         },
     ]
-
-    coco_output = {
-        "info": INFO,
-        "licenses": LICENSES,
-        "categories": CATEGORIES,
-        "images": [],
-        "annotations": []
-    }
-    return coco_output
-
-def get_paths_from_preprocessed_chips(chips_dir, glob_pattern='**/*.tif'):
-    """
-    Recursively finds the mask and image tifs in folders created
-    from PreprocessWorflow class. Source folders are of shape
-    ../chips/landsat_id/image/tile.tif and are not compatible with COCO
-    format. COCO format needed for tensorpack MaskRCNN.
-    
-    Arg:
-        root_dir: path to chips folder
-        glob_pattern: pattern to search for tif files, shouldn't need to change
-            if chips folder is used.
-    """
-    img_files = []
-    label_files = []
-    for filename in Path(chips_dir).glob(glob_pattern):
-        if 'label' not in filename.as_posix():
-            img_files.append(filename)
-        else:
-            label_files.append(filename)
-    return img_files, label_files
-
-
-
-def create_coco_json(img_files, label_files, coco_output, fileext=".tif"):
-    """
-    Creates coco json from chips folder after Preprocess workflow is done.
-    fileext argument is for creating json from tif (for training) or png (for)
-    coco annotator. coco_output comes from coco_meta()
-    """
-    image_id = 1
-    segmentation_id = 1 # every annotation needs to be unique across whole dataset
-    for image_filename, label_filename in zip(img_files, label_files):
-        img_filename = os.path.splitext(image_filename)[0]+fileext
-        binary_mask = skio.imread(label_filename)
+    return INFO_DICT, LICENSE_DICT, PRESET_CATEGORIES
         
-        image_info = pycococreatortools.create_image_info(image_id,
-                os.path.basename(img_filename), (512,512))
-        coco_output["images"].append(image_info)
-                
-        if len(binary_mask.shape) > 2:
-            for maskid in np.arange(binary_mask.shape[-1]):
-
-                category_info = {'id': 1, 'is_crowd': False}
-
-                annotation_info = pycococreatortools.create_annotation_info(
-                    segmentation_id, image_id, category_info, binary_mask[:,:,maskid],
-                    (512,512), tolerance=2)
-
-                if annotation_info is not None:
-                    coco_output["annotations"].append(annotation_info)
-
-                segmentation_id = segmentation_id + 1
-
-            image_id = image_id + 1
-
-        else:
-            image_info = pycococreatortools.create_image_info(
-                image_id, os.path.basename(img_filename), (512,512))
-            coco_output["images"].append(image_info)
-
-            category_info = {'id': 1, 'is_crowd': False}
-            
-            segmentation_id = segmentation_id + 1
-
-            annotation_info = pycococreatortools.create_annotation_info(
-                    segmentation_id, image_id, category_info, binary_mask,
-                    (512,512), tolerance=2)
-
-            if annotation_info is not None:
-                
-                coco_output["annotations"].append(annotation_info)
-
-            image_id = image_id + 1
-            
-    return coco_output # josn of all the annotation and img path info for either a train/validate or test set
-
-def save_coco_annotation(outpath, coco_output):
-    with open(outpath, 'w') as output_json_file:
-        json.dump(coco_output, output_json_file)
-    print(f"{os.path.basename(outpath)}"+" saved.")
-        
-def copy_chips(matterport_chip_paths, coco_chip_paths):
+def split_save_train_validation_test_df(tiles_path, validation_size = .1, test_size = .1, random_state = 1, save_empty_tiles = False):
     """
-    Used for copying train or test chips to new folders for COCO.
-    """
-    for m_path, c_path in zip(matterport_chip_paths, coco_chip_paths):
-        copyfile(m_path, c_path)
-        
-def split_save_train_test_df(tiles_path, save_empty_tiles = True):
-    """
-    Takes a Path to tiles and splits image and labels into train and test sets.
+    Takes a Path to tiles and randomly splits image and labels into train and test sets.
+    The test split occurs first. After, the set that is not train is randomly split into 
+    train and validation based on the validation size.
     
     Train and test csvs are saved in the coco folder that contains info on which tiles are
     empty. The df used to save these is also used by the path_df_to_coco_json function to 
-    create a coco dataset in json format in the coco folder (can be used for both train 
+    create a coco dataset in json format in the coco folder (can be used for train, validation, 
     and test df.)
     """
     image_tiles_path = tiles_path / "image_tiles"
@@ -167,17 +72,34 @@ def split_save_train_test_df(tiles_path, save_empty_tiles = True):
     if save_empty_tiles is not True:
         all_tiles_df = all_tiles_df[all_tiles_df['is_empty']==False]
 
-    train, test = train_test_split(all_tiles_df, test_size=0.1, random_state = 1)
+    not_test, test = train_test_split(all_tiles_df, test_size=test_size, random_state = 1)
+    train, validation = train_test_split(not_test, test_size=validation_size, random_state = 1)
 
     train.to_csv(tiles_path.parent / "coco" / "train.csv")
+    validation.to_csv(tiles_path.parent / "coco" / "validation.csv")
     test.to_csv(tiles_path.parent / "coco" / "test.csv")
-    return train, test
+    return train, validation, test
 
-def path_df_to_coco_json(df, split_id, coco_path):
-    print("creating coco json")
-    coco_meta = create_coco_meta(split_id)
-    coco_output = create_coco_json(df["image_tiles"].to_list(), df["label_tiles"].to_list(), coco_meta)
-    outpath = os.path.join(coco_path, f"instances_{split_id}.json")
-    save_coco_annotation(outpath, coco_output)
-    print("done saving coco json")
-    return outpath
+def create_coco_dataset(df):
+    """
+    Takes a df with paths to images and geojson tiles and creates coco formatted json for training or testing.
+    """
+    img_lst = df['image_tiles'].to_list()
+    geojson_lst = df['geojson_tiles'].to_list()
+    info, license, preset_categories = create_coco_meta()
+    # crazy regex is based on appending scene ID to each tile, including path/row and date info
+    coco_dict = sol.data.coco.geojson2coco(image_src = [str(i) for i in img_lst],
+                                       label_src = [str(i) for i in geojson_lst],
+                                       matching_re=r'(\d{6}_\d{8}_\d{8}_C\d{2}_V\d_-?\d+_\d+)',
+                                       license_dict={'CC-BY 4.0': 'https://creativecommons.org/licenses/by/4.0/'},
+                                       remove_all_multipolygons = True,
+                                       info_dict = info,
+                                       license_dict = license,
+                                       preset_categories = [preset_categories]
+                                       verbose=0)
+    return coco_dict
+
+def save_coco_annotation(outpath, coco_output):
+    with open(outpath, 'w') as output_json_file:
+        json.dump(coco_output, output_json_file)
+    print(f"{os.path.basename(outpath)}"+" saved.")
---
+++ cropmask/detectron2_reclass.py
@@ -0,0 +1,262 @@
+# This contains inherited classes from detectron2, which were defined witht he detectron2 version on January 21, 2019.
+# the changes are made to allow for logging valdiation set metrics during training and to allow for a custom data loader for tif imagery.
+
+from detectron2.data import build_detection_train_loader
+from detectron2.data import build_detection_test_loader
+from detectron2.data import transforms as T
+from detectron2.data import detection_utils as utils
+from detectron2.engine import DefaultTrainer
+from detectron2.evaluation import COCOEvaluator, DatasetEvaluators
+import copy
+import torch
+
+def tif_dataset_mapper(dataset_dict):
+    # Implement a mapper, similar to the default DatasetMapper, but with your own customizations
+    dataset_dict = copy.deepcopy(dataset_dict)  # it will be modified by code below
+    image = utils.read_image(dataset_dict["file_name"]) # relies on my own edit that updates the func to be able to read tif imagery with imageio
+#     image, transforms = T.apply_transform_gens([T.Resize((800, 800))], image)
+    dataset_dict["image"] = torch.as_tensor(image.transpose(2, 0, 1).astype("float32"))
+
+#     annos = [
+#         utils.transform_instance_annotations(obj, transforms, image.shape[:2])
+#         for obj in dataset_dict.pop("annotations")
+#         if obj.get("iscrowd", 0) == 0
+#     ]
+
+    annos = [
+        obj
+        for obj in dataset_dict.pop("annotations")
+        if obj.get("iscrowd", 0) == 0
+    ]
+    instances = utils.annotations_to_instances(annos, image.shape[:2])
+    dataset_dict["instances"] = utils.filter_empty_instances(instances)
+    return dataset_dict
+
+def build_detection_validation_loader(cfg, mapper=None):
+    """
+    A data loader is created by the following steps:
+    1. Use the dataset names in config to query :class:`DatasetCatalog`, and obtain a list of dicts.
+    2. Start workers to work on the dicts. Each worker will:
+      * Map each metadata dict into another format to be consumed by the model.
+      * Batch them by simply putting dicts into a list.
+    The batched ``list[mapped_dict]`` is what this dataloader will return.
+    Args:
+        cfg (CfgNode): the config
+        mapper (callable): a callable which takes a sample (dict) from dataset and
+            returns the format to be consumed by the model.
+            By default it will be `DatasetMapper(cfg, True)`.
+    Returns:
+        an infinite iterator of validation data
+    """
+    num_workers = get_world_size()
+    images_per_batch = cfg.SOLVER.IMS_PER_BATCH
+    assert (
+        images_per_batch % num_workers == 0
+    ), "SOLVER.IMS_PER_BATCH ({}) must be divisible by the number of workers ({}).".format(
+        images_per_batch, num_workers
+    )
+    assert (
+        images_per_batch >= num_workers
+    ), "SOLVER.IMS_PER_BATCH ({}) must be larger than the number of workers ({}).".format(
+        images_per_batch, num_workers
+    )
+    images_per_worker = images_per_batch // num_workers
+
+    dataset_dicts = get_detection_dataset_dicts(
+        cfg.DATASETS.VALIDATION, # this is the only difference between the valdiation loader and train loader.
+        filter_empty=cfg.DATALOADER.FILTER_EMPTY_ANNOTATIONS,
+        min_keypoints=cfg.MODEL.ROI_KEYPOINT_HEAD.MIN_KEYPOINTS_PER_IMAGE
+        if cfg.MODEL.KEYPOINT_ON
+        else 0,
+        proposal_files=cfg.DATASETS.PROPOSAL_FILES_TRAIN if cfg.MODEL.LOAD_PROPOSALS else None, # won't use this I think but if I do, needs to be changed for valdiation set.
+    )
+    dataset = DatasetFromList(dataset_dicts, copy=False)
+
+    if mapper is None:
+        mapper = DatasetMapper(cfg, True)
+    dataset = MapDataset(dataset, mapper)
+
+    sampler_name = cfg.DATALOADER.SAMPLER_TRAIN #valdiation should use same batching and sampling scheme as training (rather than batch of size 1)
+    logger = logging.getLogger(__name__)
+    logger.info("Using training sampler {}".format(sampler_name))
+    if sampler_name == "TrainingSampler":
+        sampler = samplers.TrainingSampler(len(dataset))
+    elif sampler_name == "RepeatFactorTrainingSampler":
+        sampler = samplers.RepeatFactorTrainingSampler(
+            dataset_dicts, cfg.DATALOADER.REPEAT_THRESHOLD
+        )
+    else:
+        raise ValueError("Unknown training sampler: {}".format(sampler_name))
+
+    if cfg.DATALOADER.ASPECT_RATIO_GROUPING:
+        data_loader = torch.utils.data.DataLoader(
+            dataset,
+            sampler=sampler,
+            num_workers=cfg.DATALOADER.NUM_WORKERS,
+            batch_sampler=None,
+            collate_fn=operator.itemgetter(0),  # don't batch, but yield individual elements
+            worker_init_fn=worker_init_reset_seed,
+        )  # yield individual mapped dict
+        data_loader = AspectRatioGroupedDataset(data_loader, images_per_worker)
+    else:
+        batch_sampler = torch.utils.data.sampler.BatchSampler(
+            sampler, images_per_worker, drop_last=True
+        )
+        # drop_last so the batch always have the same size
+        data_loader = torch.utils.data.DataLoader(
+            dataset,
+            num_workers=cfg.DATALOADER.NUM_WORKERS,
+            batch_sampler=batch_sampler,
+            collate_fn=trivial_batch_collator,
+            worker_init_fn=worker_init_reset_seed,
+        )
+
+    return data_loader
+
+class Trainer(DefaultTrainer):
+    """
+    This subclasses the Default Trainer to plot a validation loss curve alongside train loss curve in tensorboard.
+    This requires a train/validation/test split upfront.
+    """
+    def __init__(self, cfg):
+        """
+        Args:
+            cfg (CfgNode):
+        """
+        super().__init__(cfg)
+
+        validation_data_loader = self.build_validation_loader(cfg)
+        self.validation_data_loader_iter = iter(validation_data_loader)
+        
+    @classmethod
+    def build_evaluator(cls, cfg, dataset_name):
+        output_folder = os.path.join(cfg.OUTPUT_DIR, "inference")
+        evaluators = [COCOEvaluator(dataset_name, cfg, True, output_folder)]
+        return DatasetEvaluators(evaluators)
+
+    @classmethod
+    def build_test_loader(cls, cfg, dataset_name):
+        return build_detection_test_loader(cfg, dataset_name, mapper=tif_dataset_mapper)
+    
+    @classmethod
+    def build_validation_loader(cls, cfg, dataset_name):
+        return build_detection_validation_loader(cfg, mapper=tif_dataset_mapper)
+
+    @classmethod
+    def build_train_loader(cls, cfg):
+        return build_detection_train_loader(cfg, mapper=tif_dataset_mapper)
+    
+    def _write_validation_metrics(self, metrics_dict: dict):
+        """
+        Args:
+            metrics_dict (dict): dict of scalar metrics
+        """
+        metrics_dict = {
+            k: v.detach().cpu().item() if isinstance(v, torch.Tensor) else float(v)
+            for k, v in metrics_dict.items()
+        }
+        # gather metrics among all workers for logging
+        # This assumes we do DDP-style training, which is currently the only
+        # supported method in detectron2.
+        all_metrics_dict = comm.gather(metrics_dict)
+
+        if comm.is_main_process():
+            if "data_time" in all_metrics_dict[0]:
+                # data_time among workers can have high variance. The actual latency
+                # caused by data_time is the maximum among workers.
+                data_time = np.max([x.pop("data_time") for x in all_metrics_dict])
+                self.storage.put_scalar("data_time", data_time)
+
+            # average the rest metrics. val added to metric key names 
+            # compared to original _write_metrics func in train_loop.py
+            metrics_dict = {
+                "va_l"+k: np.mean([x[k] for x in all_metrics_dict]) for k in all_metrics_dict[0].keys()
+            }
+            total_losses_reduced = sum(loss for loss in metrics_dict.values())
+
+            self.storage.put_scalar("val_total_loss", total_losses_reduced)
+            if len(metrics_dict) > 1:
+                self.storage.put_scalars(**metrics_dict)
+    
+    def run_step(step):
+        """
+        Implement the standard training logic described above.
+        """
+        assert self.model.training, "[SimpleTrainer] model was changed to eval mode!"
+        start = time.perf_counter()
+        """
+        If your want to do something with the data, you can wrap the dataloader.
+        """
+        data = next(self._data_loader_iter)
+        data_time = time.perf_counter() - start
+
+        """
+        If your want to do something with the losses, you can wrap the model.
+        """
+        loss_dict = self.model(data)
+        losses = sum(loss for loss in loss_dict.values())
+        self._detect_anomaly(losses, loss_dict)
+
+        metrics_dict = loss_dict
+        metrics_dict["data_time"] = data_time
+        self._write_metrics(metrics_dict)
+        
+        """
+        Added to write out validation metrics.
+        """
+        validation_data = next(self.validation_data_loader_iter)
+        val_losses_dict = self.model(validation_data)
+        val_losses = sum(loss for loss in val_loss_dict.values())
+        self._detect_anomaly(val_losses, val_loss_dict)
+
+        val_metrics_dict = val_loss_dict
+        val_metrics_dict["data_time"] = data_time
+        self._write_validation_metrics(val_metrics_dict)
+
+        """
+        If you need accumulate gradients or something similar, you can
+        wrap the optimizer with your custom `zero_grad()` method.
+        """
+        self.optimizer.zero_grad()
+        losses.backward()
+
+        """
+        If you need gradient clipping/scaling or other processing, you can
+        wrap the optimizer with your custom `step()` method.
+        """
+        self.optimizer.step()
+        
+    def _write_validation_metrics(self, metrics_dict: dict):
+        """
+        Args:
+            metrics_dict (dict): dict of scalar metrics
+        """
+        metrics_dict = {
+            k: v.detach().cpu().item() if isinstance(v, torch.Tensor) else float(v)
+            for k, v in metrics_dict.items()
+        }
+        # gather metrics among all workers for logging
+        # This assumes we do DDP-style training, which is currently the only
+        # supported method in detectron2.
+        all_metrics_dict = comm.gather(metrics_dict)
+
+        if comm.is_main_process():
+            if "data_time" in all_metrics_dict[0]:
+                # data_time among workers can have high variance. The actual latency
+                # caused by data_time is the maximum among workers.
+                data_time = np.max([x.pop("data_time") for x in all_metrics_dict])
+                self.storage.put_scalar("data_time", data_time)
+
+            # average the rest metrics. val added to metric key names 
+            # compared to original _write_metrics func in train_loop.py
+            metrics_dict = {
+                "va_l"+k: np.mean([x[k] for x in all_metrics_dict]) for k in all_metrics_dict[0].keys()
+            }
+            total_losses_reduced = sum(loss for loss in metrics_dict.values())
+
+            self.storage.put_scalar("val_total_loss", total_losses_reduced)
+            if len(metrics_dict) > 1:
+                self.storage.put_scalars(**metrics_dict)
+
+
+
