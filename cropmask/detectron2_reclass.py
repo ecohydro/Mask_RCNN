eb0a09e7ebb64a7cab4e056a89265dbe687f9b72
Ryan Avery <ravery@ucsb.edu>
2020-01-21 22:38:13 -0800

    bug fixes, model trains but computes val losses at every step, need to change logic so that val losses computed every VAL_PERIOD

--- cropmask/detectron2_reclass.py
+++ cropmask/detectron2_reclass.py
@@ -1,36 +1,62 @@
-# This contains inherited classes from detectron2, which were defined witht he detectron2 version on January 21, 2019.
-# the changes are made to allow for logging valdiation set metrics during training and to allow for a custom data loader for tif imagery.
+# This contains inherited classes from detectron2, which were defined wit h he detectron2 version on January 21, 2019.
+# the changes are made to allow for logging validation set metrics during training and to allow for a custom data loader for tif imagery.
+import copy
+import logging
+import numpy as np
+import os
+import operator
+import torch.utils.data
+import time
 
-from detectron2.data import build_detection_train_loader, build_detection_test_loader
-import detectron2.data #for get_world_size and other things for valdiation loader
+from detectron2.utils.comm import get_world_size
+from detectron2.utils.env import seed_all_rng
+from detectron2.utils.logger import log_first_n
+
+from detectron2.data import samplers
+from detectron2.data.common import (
+    AspectRatioGroupedDataset,
+    DatasetFromList,
+    MapDataset,
+)
+from detectron2.data.dataset_mapper import DatasetMapper
+import detectron2.data.detection_utils as utils
+import detectron2.utils.comm as comm
+
+from detectron2.data.build import *
+from detectron2.data.build import (
+    worker_init_reset_seed,
+    trivial_batch_collator
+)  # it's hidden by _all_  in build.py :(
 from detectron2.data import transforms as T
-from detectron2.data import detection_utils as utils
 from detectron2.engine import DefaultTrainer
 from detectron2.evaluation import COCOEvaluator, DatasetEvaluators
-import copy
 import torch
+
 
 def tif_dataset_mapper(dataset_dict):
     # Implement a mapper, similar to the default DatasetMapper, but with your own customizations
-    dataset_dict = copy.deepcopy(dataset_dict)  # it will be modified by code below
-    image = utils.read_image(dataset_dict["file_name"]) # relies on my own edit that updates the func to be able to read tif imagery with imageio
-#     image, transforms = T.apply_transform_gens([T.Resize((800, 800))], image)
-    dataset_dict["image"] = torch.as_tensor(image.transpose(2, 0, 1).astype("float32"))
+    # it will be modified by code below
+    dataset_dict = copy.deepcopy(dataset_dict)
+    image = utils.read_image(
+        dataset_dict["file_name"]
+    )  # relies on my own edit that updates the func to be able to read tif imagery with imageio
+    #     image, transforms = T.apply_transform_gens([T.Resize((800, 800))], image)
+    dataset_dict["image"] = torch.as_tensor(
+        image.transpose(2, 0, 1).astype("float32"))
 
-#     annos = [
-#         utils.transform_instance_annotations(obj, transforms, image.shape[:2])
-#         for obj in dataset_dict.pop("annotations")
-#         if obj.get("iscrowd", 0) == 0
-#     ]
+    #     annos = [
+    #         utils.transform_instance_annotations(obj, transforms, image.shape[:2])
+    #         for obj in dataset_dict.pop("annotations")
+    #         if obj.get("iscrowd", 0) == 0
+    #     ]
 
     annos = [
-        obj
-        for obj in dataset_dict.pop("annotations")
-        if obj.get("iscrowd", 0) == 0
+        obj for obj in dataset_dict.pop("annotations") if obj.get("iscrowd", 0) == 0
     ]
     instances = utils.annotations_to_instances(annos, image.shape[:2])
     dataset_dict["instances"] = utils.filter_empty_instances(instances)
     return dataset_dict
+
 
 def build_detection_validation_loader(cfg, mapper=None):
     """
@@ -63,12 +89,16 @@ def build_detection_validation_loader(cfg, mapper=None):
     images_per_worker = images_per_batch // num_workers
 
     dataset_dicts = get_detection_dataset_dicts(
-        cfg.DATASETS.VALIDATION, # this is the only difference between the valdiation loader and train loader.
+        # this is the only difference between the valdiation loader and train loader.
+        cfg.DATASETS.VALIDATION,
         filter_empty=cfg.DATALOADER.FILTER_EMPTY_ANNOTATIONS,
         min_keypoints=cfg.MODEL.ROI_KEYPOINT_HEAD.MIN_KEYPOINTS_PER_IMAGE
         if cfg.MODEL.KEYPOINT_ON
         else 0,
-        proposal_files=cfg.DATASETS.PROPOSAL_FILES_TRAIN if cfg.MODEL.LOAD_PROPOSALS else None, # won't use this I think but if I do, needs to be changed for valdiation set.
+        proposal_files=cfg.DATASETS.PROPOSAL_FILES_TRAIN
+        if cfg.MODEL.LOAD_PROPOSALS
+        # won't use this I think but if I do, needs to be changed for valdiation set.
+        else None,
     )
     dataset = DatasetFromList(dataset_dicts, copy=False)
 
@@ -76,7 +106,9 @@ def build_detection_validation_loader(cfg, mapper=None):
         mapper = DatasetMapper(cfg, True)
     dataset = MapDataset(dataset, mapper)
 
-    sampler_name = cfg.DATALOADER.SAMPLER_TRAIN #valdiation should use same batching and sampling scheme as training (rather than batch of size 1)
+    sampler_name = (
+        cfg.DATALOADER.SAMPLER_TRAIN
+    )  # valdiation should use same batching and sampling scheme as training (rather than batch of size 1)
     logger = logging.getLogger(__name__)
     logger.info("Using training sampler {}".format(sampler_name))
     if sampler_name == "TrainingSampler":
@@ -94,7 +126,9 @@ def build_detection_validation_loader(cfg, mapper=None):
             sampler=sampler,
             num_workers=cfg.DATALOADER.NUM_WORKERS,
             batch_sampler=None,
-            collate_fn=operator.itemgetter(0),  # don't batch, but yield individual elements
+            collate_fn=operator.itemgetter(
+                0
+            ),  # don't batch, but yield individual elements
             worker_init_fn=worker_init_reset_seed,
         )  # yield individual mapped dict
         data_loader = AspectRatioGroupedDataset(data_loader, images_per_worker)
@@ -113,11 +147,13 @@ def build_detection_validation_loader(cfg, mapper=None):
 
     return data_loader
 
+
 class Trainer(DefaultTrainer):
     """
     This subclasses the Default Trainer to plot a validation loss curve alongside train loss curve in tensorboard.
     This requires a train/validation/test split upfront.
     """
+
     def __init__(self, cfg):
         """
         Args:
@@ -127,7 +163,7 @@ class Trainer(DefaultTrainer):
 
         validation_data_loader = self.build_validation_loader(cfg)
         self.validation_data_loader_iter = iter(validation_data_loader)
-        
+
     @classmethod
     def build_evaluator(cls, cfg, dataset_name):
         output_folder = os.path.join(cfg.OUTPUT_DIR, "inference")
@@ -137,7 +173,7 @@ class Trainer(DefaultTrainer):
     @classmethod
     def build_test_loader(cls, cfg, dataset_name):
         return build_detection_test_loader(cfg, dataset_name, mapper=tif_dataset_mapper)
-    
+
     @classmethod
     def build_validation_loader(cls, cfg):
         return build_detection_validation_loader(cfg, mapper=tif_dataset_mapper)
@@ -145,40 +181,8 @@ class Trainer(DefaultTrainer):
     @classmethod
     def build_train_loader(cls, cfg):
         return build_detection_train_loader(cfg, mapper=tif_dataset_mapper)
-    
-    def _write_validation_metrics(self, metrics_dict: dict):
-        """
-        Args:
-            metrics_dict (dict): dict of scalar metrics
-        """
-        metrics_dict = {
-            k: v.detach().cpu().item() if isinstance(v, torch.Tensor) else float(v)
-            for k, v in metrics_dict.items()
-        }
-        # gather metrics among all workers for logging
-        # This assumes we do DDP-style training, which is currently the only
-        # supported method in detectron2.
-        all_metrics_dict = comm.gather(metrics_dict)
 
-        if comm.is_main_process():
-            if "data_time" in all_metrics_dict[0]:
-                # data_time among workers can have high variance. The actual latency
-                # caused by data_time is the maximum among workers.
-                data_time = np.max([x.pop("data_time") for x in all_metrics_dict])
-                self.storage.put_scalar("data_time", data_time)
-
-            # average the rest metrics. val added to metric key names 
-            # compared to original _write_metrics func in train_loop.py
-            metrics_dict = {
-                "va_l"+k: np.mean([x[k] for x in all_metrics_dict]) for k in all_metrics_dict[0].keys()
-            }
-            total_losses_reduced = sum(loss for loss in metrics_dict.values())
-
-            self.storage.put_scalar("val_total_loss", total_losses_reduced)
-            if len(metrics_dict) > 1:
-                self.storage.put_scalars(**metrics_dict)
-    
-    def run_step(step):
+    def run_step(self):
         """
         Implement the standard training logic described above.
         """
@@ -200,16 +204,16 @@ class Trainer(DefaultTrainer):
         metrics_dict = loss_dict
         metrics_dict["data_time"] = data_time
         self._write_metrics(metrics_dict)
-        
+
         """
         Added to write out validation metrics.
         """
         validation_data = next(self.validation_data_loader_iter)
         val_losses_dict = self.model(validation_data)
-        val_losses = sum(loss for loss in val_loss_dict.values())
-        self._detect_anomaly(val_losses, val_loss_dict)
+        val_losses = sum(loss for loss in val_losses_dict.values())
+        self._detect_anomaly(val_losses, val_losses_dict)
 
-        val_metrics_dict = val_loss_dict
+        val_metrics_dict = val_losses_dict
         val_metrics_dict["data_time"] = data_time
         self._write_validation_metrics(val_metrics_dict)
 
@@ -225,7 +229,7 @@ class Trainer(DefaultTrainer):
         wrap the optimizer with your custom `step()` method.
         """
         self.optimizer.step()
-        
+
     def _write_validation_metrics(self, metrics_dict: dict):
         """
         Args:
@@ -244,19 +248,18 @@ class Trainer(DefaultTrainer):
             if "data_time" in all_metrics_dict[0]:
                 # data_time among workers can have high variance. The actual latency
                 # caused by data_time is the maximum among workers.
-                data_time = np.max([x.pop("data_time") for x in all_metrics_dict])
+                data_time = np.max([x.pop("data_time")
+                                    for x in all_metrics_dict])
                 self.storage.put_scalar("data_time", data_time)
 
-            # average the rest metrics. val added to metric key names 
+            # average the rest metrics. val added to metric key names
             # compared to original _write_metrics func in train_loop.py
             metrics_dict = {
-                "va_l"+k: np.mean([x[k] for x in all_metrics_dict]) for k in all_metrics_dict[0].keys()
+                "val_" + k: np.mean([x[k] for x in all_metrics_dict])
+                for k in all_metrics_dict[0].keys()
             }
             total_losses_reduced = sum(loss for loss in metrics_dict.values())
 
             self.storage.put_scalar("val_total_loss", total_losses_reduced)
             if len(metrics_dict) > 1:
                 self.storage.put_scalars(**metrics_dict)
-
-
-
