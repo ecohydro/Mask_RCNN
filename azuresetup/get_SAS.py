4657ed1d103acb37dc974aa6af2f0d3a3398e987
Ryan Avery <ravery@ucsb.edu>
2021-03-16 15:00:07 -0700

    cleanup old unused stuff and ignore test stuff

--- azuresetup/get_SAS.py
+++
@@ -1,3 +0,0 @@
-https://docs.microsoft.com/en-us/azure/storage/common/storage-dotnet-shared-access-signature-part-1?toc=%2fazure%2fstorage%2fblobs%2ftoc.json
-
-https://github.com/Azure/azure-storage-python/blob/master/samples/blob/sas_usage.py
--- scripts/setup_postgisdb.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-export PGPORT=5434
-export PGHOST=localhost
-export PGUSER=admin
-export PGPASSWORD=password
-export PGDATABASE=postgis
